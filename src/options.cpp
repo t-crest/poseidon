2c36ee746eaaeef388a342c5282b2a3c388b820b
Mark Ruvald Pedersen <wabsie@gmail.com>
2012-04-17 15:44:24 +0200

    BAD_RANDOM

--- src/options.cpp
+++ src/options.cpp
@@ -34,11 +34,12 @@ options::options(int argc, char *argv[])
 options::meta_t options::parse_meta_t(string str) 
 {
 	meta_t ret = ERR;
-	if		(str=="RANDOM")	ret=RANDOM;
-	else if (str=="GREEDY")	ret=GREEDY;
-	else if (str=="rGREEDY")ret=rGREEDY;
-	else if (str=="GRASP")	ret=GRASP;		
-	else if (str=="ALNS")	ret=ALNS;
+	if		(str=="RANDOM")		ret=RANDOM;
+	else if	(str=="BAD_RANDOM")	ret=BAD_RANDOM;
+	else if (str=="GREEDY")		ret=GREEDY;
+	else if (str=="rGREEDY")	ret=rGREEDY;
+	else if (str=="GRASP")		ret=GRASP;		
+	else if (str=="ALNS")		ret=ALNS;
 	return ret;
 }
 
