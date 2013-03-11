da8623e97151a88521a2c17ad4ef6fecf59012a0
Rasmus Bo Sørensen <rasmus@rbscloud.dk>
2013-03-11 17:10:29 +0100

    File added.

---
+++ MCSL/create_xml.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+EXE="./bin/MCSL"
+TRAFFIC_DIR=./MCSL_NoC_Benchmark_Suite_v1.5/traffic
+XML_DIR=./xml
+APPLICATIONS="Sparse FFT-1024_complex Fpppp Robot RS-32_28_8_dec RS-32_28_8_enc H264-720p_dec"
+
+for t in torus mesh ; do
+	rm -rf ${XML_DIR}/${t} 2>/dev/null
+	rm -rf ${XML_DIR}/${t} 2>/dev/null
+	mkdir ${XML_DIR}/${t} 2>/dev/null
+	mkdir ${XML_DIR}/${t} 2>/dev/null
+	for i in {3..16} ; do
+		echo "------------" ;
+		echo ${i}x${i}_${t} ;
+		echo "------------" ;
+		mkdir ${XML_DIR}/${t}/${t}_${i}x${i} ;
+		for a in ${APPLICATIONS} ; do
+			${EXE} -f "${TRAFFIC_DIR}/${t}/${t}_${i}x${i}/${a}_${t}_${i}x${i}.stp" \
+			-o "${XML_DIR}/${t}/${t}_${i}x${i}/${a}_${t}_${i}x${i}_max_rate.stp.xml"
+			${EXE} -f "${TRAFFIC_DIR}/${t}/${t}_${i}x${i}/${a}_${t}_${i}x${i}.stp" \
+			-m -o "${XML_DIR}/${t}/${t}_${i}x${i}/${a}_${t}_${i}x${i}_mean.stp.xml"
+		done
+	done
+done
