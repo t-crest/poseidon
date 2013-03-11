516dcdc568967d52db0a9c9b5e81be109971e49f
Rasmus Bo Sørensen <rasmus@rbscloud.dk>
2013-03-11 10:31:59 +0100

    Link utilization added to results script.

--- MCSL_results.sh
+++ MCSL_results.sh
@@ -27,11 +27,12 @@ for t in mesh torus ; do
 			then
 				min=$(cat stat*${t}_${t}_${s}x${s}_${a}* 2>/dev/null | awk '{if (NF==5) print $3}  {if (NF==2) print $2}' | sort -nur | tail -1)
 				i=$(cat stat*${t}_${t}_${s}x${s}_${a}* 2>/dev/null | awk '{if (NF==5) print $4}' | sort -nu | tail -1)
+				util=$(cat stat*${t}_${t}_${s}x${s}_${a}* 2>/dev/null | grep -ERi Link_util_best | awk '{print $3}' | sort -nu | tail -1)
 				if [ "$i" != "" ]; then
 					iter="Iterations "
 				fi
 				if [ "$min" != "" ]; then
-					echo -e "Found $min \tfor ${t}${s}x${s}_${a} ${m}\t$iter$i" 
+					echo -e "Found $min \tfor ${t}${s}x${s}_${a} ${m}\t$iter$i \tLink_util ${util}" 
 				else
 					if [ "$1" != "-c" ]; then
 						echo -e "Pending... \tfor ${t}${s}x${s}_${a} ${m}" 
