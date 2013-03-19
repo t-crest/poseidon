ef6350c206fd904c7b465299e6ea4df169ec8b9b
Rasmus Bo Sørensen <rasmus@rbscloud.dk>
2013-03-19 13:18:05 +0100

    test: the sigma test have been expanded.

--- MCSL/test/MCSL_test.sh
+++ MCSL/test/MCSL_test.sh
@@ -2,10 +2,10 @@
 GRAMME=true
 DATA_DIR="./xml_in"
 PROG=snts
-TOPO=torus
-SIZE=16
+#TOPO=torus
+#SIZE=16
 command -v nproc >/dev/null && CORES=$(nproc) || CORES=32
-TORUS_APPLICATIONS="RS-32_28_8_dec RS-32_28_8_enc FFT-1024_complex Sparse Robot Fpppp H264-720p_dec"
+APPLICATIONS="RS-32_28_8_dec RS-32_28_8_enc FFT-1024_complex Sparse Robot Fpppp H264-720p_dec"
 if [ "${GRAMME}" = "true" ]
 then
 	RUNFOR=7200
@@ -15,35 +15,36 @@ else
 	FULL_PROG="../../dist/Release/Cygwin_4.x-Windows/${PROG}"
 fi
 
-
-
 function run {
 #	echo "Queing $FULL_PROG $@"
-	sleep $(( $RANDOM % 15 )).$(( $RANDOM % 1000 ))
+#	sleep $(( $RANDOM % 1 )).$(( $RANDOM % 1000 ))
 
 	while [ $(pgrep snts | wc -l) -ge $CORES ] ; do 
 		sleep $(( $RANDOM % 5 )).$(( $RANDOM % 1000 ))
 	done
 
 	echo "Executing $FULL_PROG $@"	
-	$FULL_PROG $@
+	$FULL_PROG $@ &
 }
 
-for a in ${TORUS_APPLICATIONS} ; do
-	
-	for s in 1 1.1 1.2 1.3 1.5 2 3 4 5 10 20 30 50 100 200 300 500 ; do
-		# Single shot solutions
-#		for m in GREEDY rGREEDY; do
-		for m in GREEDY ; do
-			run "-f ${DATA_DIR}/${a}/${TOPO}/${TOPO}_${SIZE}x${SIZE}/${a}_s_${s}.xml -m ${m}" &
-		done 	
+for a in ${APPLICATIONS} ; do
+	for topo in torus ; do # turus mesh
+		for size in 8 16 ; do
+			for s in 1 1.2 1.5 2 3 5 10 20 30 50 100 200 300 500 ; do
+				# Single shot solutions
+		#		for m in GREEDY rGREEDY; do
+				for m in GREEDY ; do
+					run "-f ${DATA_DIR}/${a}/${topo}/${topo}_${size}x${size}/${a}_s_${s}.xml -m ${m}"
+				done 	
 
-		for b in 0.01 0.02 0.1 0.2 ; do
-		 	run "-f ${DATA_DIR}/${a}/${TOPO}/${TOPO}_${SIZE}x${SIZE}/${a}_s_${s}.xml -m GRASP -t $RUNFOR -b ${b}" &
-		done 
+				for b in 0.02 0.2 ; do
+				 	run "-f ${DATA_DIR}/${a}/${topo}/${topo}_${size}x${size}/${a}_s_${s}.xml -m GRASP -t $RUNFOR -b ${b}"
+				done 
 
-		for i in GREEDY rGREEDY ; do
-		 	run "-f ${DATA_DIR}/${a}/${TOPO}/${TOPO}_${SIZE}x${SIZE}/${a}_s_${s}.xml -m ALNS -t $RUNFOR -i ${i}" &
-		done 
+				for i in GREEDY ; do #rGREEDY
+				 	run "-f ${DATA_DIR}/${a}/${topo}/${topo}_${size}x${size}/${a}_s_${s}.xml -m ALNS -t $RUNFOR -i ${i}"
+				done 
+			done
+		done
 	done
 done
--- MCSL/test/sigma_test.sh
+++ MCSL/test/sigma_test.sh
@@ -7,11 +7,13 @@ NETWORK_SIZE=16
 
 
 for a in ${APPLICATIONS} ; do
-	mkdir ${OUTPUT_DIR}/${a} 2>/dev/null
-	mkdir ${OUTPUT_DIR}/${a}/torus 2>/dev/null
-	for si in 1 1.1 1.2 1.3 1.5 2 3 4 5 10 20 30 50 100 200 300 500 ; do
-		mkdir ${OUTPUT_DIR}/${a}/torus/torus_${NETWORK_SIZE}x${NETWORK_SIZE} 2>/dev/null
-		$FULL_PROG -f "${TRAFFIC_DIR}/torus/torus_${NETWORK_SIZE}x${NETWORK_SIZE}/${a}_torus_${NETWORK_SIZE}x${NETWORK_SIZE}.stp" \
-		-o "${OUTPUT_DIR}/${a}/torus/torus_${NETWORK_SIZE}x${NETWORK_SIZE}/${a}_s_${si}.xml" -s "${si}"
+	for topo in torus ; do # turus mesh
+		for size in 8 16 ; do
+			for si in 1 1.2 1.5 2 3 5 10 20 30 50 100 200 300 500 ; do
+				mkdir -p ${OUTPUT_DIR}/${a}/${topo}/${topo}_${size}x${size} 2>/dev/null
+				$FULL_PROG -f "${TRAFFIC_DIR}/${topo}/${topo}_${size}x${size}/${a}_${topo}_${size}x${size}.stp" \
+				-o "${OUTPUT_DIR}/${a}/${topo}/${topo}_${size}x${size}/${a}_s_${si}.xml" -s "${si}"
+			done
+		done
 	done
 done
