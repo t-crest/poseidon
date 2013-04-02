bb3b1e393713d87d15db2d7b4ce2d92fa4339fec
Rasmus Bo Sørensen <rasmus@rbscloud.dk>
2013-04-02 09:14:39 +0200

    All-to-all test script changed.

--- test.sh
+++ test.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
-
-command -v nproc >/dev/null && cores=$(nproc) || cores=32
-runfor=7200
-prog=metanoc
-full_prog="metanoc/dist/Gramme/GNU-Linux-x86/${prog}"
+GRAMME=true
+PROG=snts
+command -v nproc >/dev/null && CORES=$(nproc) || CORES=32
+if [ "${GRAMME}" = "true" ]
+then
+	RUNFOR=36000
+	FULL_PROG="../../dist/Gramme/GNU-Linux-x86/snts"
+else
+	RUNFOR=10
+	FULL_PROG="../../dist/Release/Cygwin_4.x-Windows/${PROG}"
+fi
 
 function run {
-#	echo "Queing $full_prog $@"
-	sleep $(( $RANDOM % 15 )).$(( $RANDOM % 1000 ))
+#	echo "Queing $FULL_PROG $@"
+#	sleep $(( $RANDOM % 1 )).$(( $RANDOM % 1000 ))
 
-	while [ $(pgrep metanoc | wc -l) -ge $cores ] ; do 
+	while [ $(pgrep snts | wc -l) -ge $CORES ] ; do 
 		sleep $(( $RANDOM % 5 )).$(( $RANDOM % 1000 ))
 	done
 
-	echo "Executing $full_prog $@"	
-	$full_prog $@
+	echo "Executing $FULL_PROG $@"	
+	$FULL_PROG $@ &
 }
 
-
-
 for t in bitorus mesh ; do
-	for s in 3 4 5 6 7 8 9 10 15 ; do 
+	for s in 15 ; do # 3 4 5 6 7 8 9 10 15
 		# Single shot solutions
-		for m in GREEDY rGREEDY RANDOM BAD_RANDOM ; do
+		for m in GREEDY rGREEDY ; do # RANDOM BAD_RANDOM
 			run "-f ./data/${t}${s}x${s}.xml -m ${m}" &
 		done 	
 
 		for b in 0 0.01 0.02 0.05 0.1 0.2 0.3 0.5 0.9 1.0 ; do
-			run "-f ./data/${t}${s}x${s}.xml -m GRASP -t $runfor -b ${b}" &
+			run "-f ./data/${t}${s}x${s}.xml -m GRASP -t $RUNFOR -b ${b}" &
 		done 
 
-		for i in GREEDY rGREEDY RANDOM BAD_RANDOM ; do
-			run "-f ./data/${t}${s}x${s}.xml -m ALNS -t $runfor -i ${i}" &
+		for i in GREEDY rGREEDY RANDOM ; do # BAD_RANDOM
+			run "-f ./data/${t}${s}x${s}.xml -m ALNS -t $RUNFOR -i ${i}" &
 		done 
 	done 
 done
