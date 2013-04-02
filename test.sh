70c03657645e976480263fc372607b8d5a729aca
Rasmus Bo Sørensen <rasmus@rbscloud.dk>
2013-04-02 09:17:14 +0200

    process detach removed in test.sh script

--- test.sh
+++ test.sh
@@ -27,15 +27,15 @@ for t in bitorus mesh ; do
 	for s in 15 ; do # 3 4 5 6 7 8 9 10 15
 		# Single shot solutions
 		for m in GREEDY rGREEDY ; do # RANDOM BAD_RANDOM
-			run "-f ./data/${t}${s}x${s}.xml -m ${m}" &
+			run "-f ./data/${t}${s}x${s}.xml -m ${m}"
 		done 	
 
 		for b in 0 0.01 0.02 0.05 0.1 0.2 0.3 0.5 0.9 1.0 ; do
-			run "-f ./data/${t}${s}x${s}.xml -m GRASP -t $RUNFOR -b ${b}" &
+			run "-f ./data/${t}${s}x${s}.xml -m GRASP -t $RUNFOR -b ${b}"
 		done 
 
 		for i in GREEDY rGREEDY RANDOM ; do # BAD_RANDOM
-			run "-f ./data/${t}${s}x${s}.xml -m ALNS -t $RUNFOR -i ${i}" &
+			run "-f ./data/${t}${s}x${s}.xml -m ALNS -t $RUNFOR -i ${i}"
 		done 
 	done 
 done
