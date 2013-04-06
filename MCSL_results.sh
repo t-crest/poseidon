7caa81ac87bf7ea02c9b8c44c8dfb935f0875bd4
Rasmus Bo Sørensen <rasmus@rbscloud.dk>
2013-04-06 14:08:52 +0200

    MCSL result script changed to only display max rate tests.

--- MCSL_results.sh
+++ MCSL_results.sh
@@ -6,29 +6,30 @@ then
 	unset RESULTS
 fi
 
-for t in mesh torus ; do 
-	for a in ${APPLICATIONS} ; do
-		for s in 3 4 5 6 7 8 9 10 11 12 13 14 15 16 ; do 
-			for m in ${RESULTS} ; do
-				min=$(cat stat*${t}_${t}_${s}x${s}_${a}*${m}* 2>/dev/null | awk '{if (NF==5) print $3}  {if (NF==2) print $2}' | sort -nur | tail -1)
-				i=$(cat stat*${t}_${t}_${s}x${s}_${a}*${m}* 2>/dev/null | awk '{if (NF==5) print $4}' | sort -nu | tail -1)
-				if [ "$i" != "" ]; then
-					iter="Iterations "
-				fi
-				if [ "$min" != "" ]; then
-					echo -e "Found $min \tfor ${t}${s}x${s}_${a} ${m}\t$iter$i" 
-				else
-					if [ "$1" != "-c" ]; then
-						echo -e "Pending... \tfor ${t}${s}x${s}_${a} ${m}" 
+for r in max_rate ; do
+	for t in mesh torus ; do 
+		for a in ${APPLICATIONS} ; do
+			for s in 3 4 5 6 7 8 9 10 11 12 13 14 15 16 ; do 
+				for m in ${RESULTS} ; do
+					min=$(cat stat*${t}_${t}_${s}x${s}_${a}*${r}*${m}* 2>/dev/null | awk '{if (NF==5) print $3}  {if (NF==2) print $2}' | sort -nur | tail -1)
+					i=$(cat stat*${t}_${t}_${s}x${s}_${a}*${r}*${m}* 2>/dev/null | awk '{if (NF==5) print $4}' | sort -nu | tail -1)
+					if [ "$i" != "" ]; then
+						iter="Iterations "
 					fi
-				fi
+					if [ "$min" != "" ]; then
+						echo -e "Found $min \tfor ${t}${s}x${s}_${a} ${m}\t$iter$i" 
+					else
+						if [ "$1" != "-c" ]; then
+							echo -e "Pending... \tfor ${t}${s}x${s}_${a} ${m}" 
+						fi
+					fi
+				done
 			done
 		done
 	done
 done
 
-
-for r in mean max_rate ; do
+for r in max_rate ; do
 	for t in mesh torus ; do 
 		for a in ${APPLICATIONS} ; do
 			for s in 3 4 5 6 7 8 9 10 11 12 13 14 15 16 ; do 
